393fd92523fd631ada1e13d597fcbf66891a5974
Flavian Kaufmann <flavian112@me.com>
2018-07-18 15:16:03 +0200

    Load img file to memory

--- src/LoadImg.py
+++ src/LoadImg.py
@@ -1,9 +1,20 @@
 import cv2
 import numpy as np
-
+import os
 
 
 def loadImg(path, size=(0,0), scale=(1,1)):
     loadedImg = cv2.imread(path)
     resizedImg = cv2.resize(loadedImg, size, fx=scale[0], fy=scale[1])
     return resizedImg
+
+def loadImgFromDir(dir):
+    imgs = []
+    for imgname in sorted(os.listdir(dir)):
+        img = cv2.imread(os.path.join(dir,imgname))
+        if img is not None:
+            imgs.append(img)
+    return imgs
+
+def rmFile(path):
+    os.remove(path)
